ea6263f2c9935aa13515ae163fc638b357d82f0a
Martin Wiechmann <mwiechmann@techfak.uni-bielefeld.de>
2018-03-19 20:39:07 +0100

    reworked markdown module for python3 compatibility: added UTF-8 decoding.
    Signed-off-by: Martin Wiechmann <mwiechmann@techfak.uni-bielefeld.de>

--- citkat/modules/markdown_content/__init__.py
+++ citkat/modules/markdown_content/__init__.py
@@ -2,7 +2,9 @@ from flask import Blueprint, Markup, render_template, abort, current_app, safe_j
 from markdown import Markdown
 # from markdown_blockdiag import BlockdiagExtension
 
-markdown_content_blueprint = Blueprint(name='markdown_content', import_name=__name__, url_prefix='/content')
+markdown_content_blueprint = Blueprint(
+    name='markdown_content', import_name=__name__, url_prefix='/content')
+# TODO: add custom template to include all meta informations
 
 
 @markdown_content_blueprint.route('/<path:fullpath>/')
@@ -14,19 +16,28 @@ def markdown_content(fullpath):
             content = Markup("<h1>Welcome!</h1>")
             return render_template('layout.html', **locals())
         return abort(500, warning)
-    md = Markdown(extensions=['markdown.extensions.extra',
-                              'markdown.extensions.toc',
-                              'markdown.extensions.meta',
-                              # BlockdiagExtension(format='svg')
-                              ])
+
+    extensions = [
+        'markdown.extensions.extra',
+        'markdown.extensions.toc',
+        'markdown.extensions.meta',
+        # BlockdiagExtension(format='svg')
+    ]
+    md = Markdown(extensions=extensions, output_format='html5')
+
     try:
-        f = open(safe_join(current_app.config['content-directory'], fullpath) + '.md', 'rb')
+        f = open(
+            safe_join(current_app.config['content-directory'], fullpath) +
+            '.md', 'rb')
     except IOError:
         abort(404)
-    else:
-        with f:
-            content = Markup(md.convert(f.read()))
-            title = 'Undefined'
-            if 'title' in md.Meta:
-                title = md.Meta['title'][0]
-            return render_template('layout.html', **locals())
+
+    raw_md = f.read().decode("UTF-8")
+
+    html = md.convert(raw_md)
+    title = 'Untitled Page'
+    if 'title' in md.Meta:
+        title = md.Meta['title'][0]
+    content = Markup(html)
+    return render_template(
+        'layout.html', content=content, title=title, meta=md.Meta)
